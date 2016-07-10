5befc601986d3d8f24af860bc43ddddfd085e733
Loïc Vial <contact.coyl@gmail.com>
2016-07-10 17:51:59 +0200

    Tunin

--- from-cache2.sh
+++ from-cache2.sh
@@ -1,10 +1,1 @@
-#/usr/bin/env bash
-
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 <tmp file>"
-    exit 1
-fi
-
-cd wmt14-metrics-task
-./compute-system-correlations --metrics $1 --human human-2014-05-16.scores --directions de-en fr-en ru-en --tablefmt plain --samples human-2014-05-16.folded/*.human.scores | grep tuning | awk '{print $5}'
-
+from-cache-wmt13.sh
